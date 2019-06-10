26d7e1a810c0763c6dcb661f479ea43a72e3fd96
Nikema Prophet <nikema@gmail.com>
2019-06-10 15:44:05 -0700

    Finished exercises in all three files, checked off the list in README.md

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -64,7 +64,7 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
 const car33 = inventory.find( car => car.id === 33)
-console.log(car33)
+// console.log(car33)
 console.log(`Car 33 is a ${car33.car_year} ${car33.car_make} ${car33.car_model}`);
 
 // ==== Challenge 2 ====
